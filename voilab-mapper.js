e4a958fd903dd7235f04aba344375ef63c07dc23
Joel Poulin <joel@voilab.ch>
2016-07-15 19:09:38 +0200

    *** potentially breaking commit *** - added an options (third) parameter with - strict mode (will map only values available in provided record) - some sugar option when recursing

--- voilab-mapper.js
+++ voilab-mapper.js
@@ -7,15 +7,21 @@
      *
      * @param {Object} record
      * @param {Object|Function} mapper
+     * @param {Object} options
      * @returns {Object}
      */
-    var map = function (record, mapper) {
+    var map = function (record, mapper, options) {
         var lodash = require('lodash'),
-            mapper_supplementary_args = Array.prototype.slice.call(arguments, 2);
+            mapper_supplementary_args = Array.prototype.slice.call(arguments, 3);
+
+        options = lodash.merge({
+            strict: false,
+            recurseOnBaseRecord: false
+        }, (options || {}));
 
         if (lodash.isArray(record)) {
             return lodash.map(record, function (u) {
-                return map.apply(map, [u, mapper].concat(mapper_supplementary_args));
+                return map.apply(map, [u, mapper, options].concat(mapper_supplementary_args));
             });
         }
 
@@ -31,29 +37,49 @@
         }
 
         return lodash.transform(mapper, function (acc, value, key) {
+            var callback_result;
+
             if (value === true) {
+                if (options.strict && record[key] === undefined) {
+                    return true;
+                }
                 acc[key] = record[key];
                 return true;
             }
             if (lodash.isString(value)) {
                 if (record) {
                     if (lodash.isFunction(record[value])) {
-                        acc[key] = record[value]();
+                        callback_result = record[value]();
+                        if (options.strict && callback_result === undefined) {
+                            return true;
+                        }
+                        acc[key] = callback_result;
                     } else {
+                        if (options.strict && lodash.get(record, value) === undefined) {
+                            return true;
+                        }
                         lodash.set(acc, key, lodash.get(record, value, null));
                     }
                 }
                 return true;
             }
             if (lodash.isFunction(value)) {
-                acc[key] = value(record);
+                callback_result = value(record);
+                if (options.strict && (callback_result === undefined || (lodash.isObject(callback_result) && lodash.isEmpty(callback_result)))) {
+                    return true;
+                }
+                acc[key] = callback_result;
                 return true;
             }
 
 
             // recursion
             if (lodash.isObject(value)) {
-                acc[key] = map(record[key], value);
+                callback_result = map((options.recurseOnBaseRecord ? record : record[key]), value, options);
+                if (options.strict && (callback_result === undefined || lodash.isEmpty(callback_result))) {
+                    return true;
+                }
+                acc[key] = callback_result;
             }
         }, {});
     };
